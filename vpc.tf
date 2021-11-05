5f82bf2132f337347afa85582aae30328a8a7b61
Kenneth Roper <kennethdroper@googlemail.com>
2021-11-05 13:15:09 +0000

    Added missing vars

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,19 @@
-subnets = [
-  {
-    subnet_name	= "kenneth-subnet"
-    subnet_ip	= "10.100.10.0/24"
-    subnet_region	= var.region
-  }
-]
+module "network" {
+  source  = "terraform-google-modules/network/google"
+  version = "4.0.0"
+  
+  # insert the 3 required variables here
+  network_name = "kenneth-subnet"
+  project_id = var.project
+
+  subnets = [
+    {
+      subnet_name	= "kenneth-subnet"
+      subnet_ip	= "10.100.10.0/24"
+      subnet_region	= var.region
+    }
+  ]
+
+}
+
+
